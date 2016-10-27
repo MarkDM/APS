0696ad863883f43a6e752d865b8c3d030df9a075
MarkDM <marcusdgls96@gmail.com>
2016-10-27 00:54:37 -0200

    Commit Inicial APS

---
+++ src/jogo/Cenario1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogo;
+
+import jplay.Scene;
+import jplay.URL;
+import jplay.Window;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Cenario1 {
+
+    private Window janela;
+    private Scene cena;
+    private Jogador jogador;
+
+    public Cenario1(Window janela) {
+        this.janela = janela;
+        cena = new Scene();
+        cena.loadFromFile(URL.scenario("Cenario1.scn"));
+        jogador = new Jogador(640,350);
+        run();
+    }
+
+    private void run() {
+        while (true) {
+            cena.draw();
+            jogador.draw();
+            jogador.mover(janela);
+            janela.update();
+            
+        }
+    }
+
+}
---
+++ src/jogo/Main.java
@@ -0,0 +1,30 @@
+package jogo;
+
+import javax.swing.JOptionPane;
+import jplay.GameImage;
+import jplay.Keyboard;
+import jplay.URL;
+import jplay.Window;
+
+/**
+ *
+ * @author Marcos
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Window janela = new Window(800, 600);
+        GameImage plano = new GameImage(URL.sprite("menu.png"));
+        Keyboard teclado = janela.getKeyboard();
+        
+        while(true) {
+            plano.draw();
+            janela.update();
+            
+            if (teclado.keyDown(Keyboard.ENTER_KEY)) {
+                new Cenario1(janela);
+            }
+        }
+    }
+
+}
