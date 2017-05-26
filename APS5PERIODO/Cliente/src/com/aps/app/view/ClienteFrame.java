8bb0311cfba6c3105c1ded04e73e2e84a60d37f5
Marcos Douglas <marcusdgls96@gmail.com>
2017-05-25 22:54:26 -0300

    add

--- APS5PERIODO/Cliente/src/com/aps/app/view/ClienteFrame.java
+++ APS5PERIODO/Cliente/src/com/aps/app/view/ClienteFrame.java
@@ -31,14 +31,14 @@ import javax.swing.ListSelectionModel;
  * @author Marcos
  */
 public class ClienteFrame extends TelaChat {
-    
+
     private Socket socket;
     private ChatMessage message;
     private ClienteService service;
     private Map<String, ChatPrivado> conversasPrivadas = new HashMap<String, ChatPrivado>();
     private long tamanhoPermitidoKB = 5120; //Igual a 5MB
     private boolean temArquivo = false;
-    
+
     private String nomeArquivo;
     private byte[] conteudoArquivo;
 
@@ -49,11 +49,11 @@ public class ClienteFrame extends TelaChat {
         initComponents();
         this.setLocationRelativeTo(null);
     }
-    
+
     private class ListenerSocket implements Runnable {
-        
+
         private ObjectInputStream input;
-        
+
         public ListenerSocket(Socket socket) {
             try {
                 input = new ObjectInputStream(socket.getInputStream());
@@ -61,22 +61,22 @@ public class ClienteFrame extends TelaChat {
                 Logger.getLogger(ClienteFrame.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
         @Override
         public void run() {
             ChatMessage message = null;
-            
+
             try {
                 while ((message = (ChatMessage) input.readObject()) != null) {
                     Action action = message.getAction();
-                    
+
                     if (action.equals(action.CONNECT)) {
                         connected(message);
                     } else if (action.equals(action.DISCONECT)) {
                         socket.close();
                     } else if (action.equals(action.SEND_ONE)) {
                         receive(message);
-                        
+
                     } else if (action.equals(action.USERS_ONLINE)) {
                         refreshOnlines(message);
                     }
@@ -87,20 +87,20 @@ public class ClienteFrame extends TelaChat {
                 Logger.getLogger(ClienteFrame.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
     }
-    
+
     private void refreshOnlines(ChatMessage message) {
         Set<String> names = message.getSetOnline();
         names.remove((String) message.getName());
-        
+
         String[] array = (String[]) names.toArray(new String[names.size()]);
-        
+
         this.listOnlines.setListData(array);
         this.listOnlines.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         this.listOnlines.setLayoutOrientation(JList.VERTICAL);
     }
-    
+
     public void receive(ChatMessage message) {
         if (message.getConteudoArquivo() != null) {
             try {
@@ -109,7 +109,7 @@ public class ClienteFrame extends TelaChat {
                 Logger.getLogger(ClienteFrame.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
         if (message.getAction().equals(Action.SEND_ONE)) {
 
             /*
@@ -117,7 +117,7 @@ public class ClienteFrame extends TelaChat {
              caso houver apenas chama o receive() do chat privado
              */
             for (Map.Entry<String, ChatPrivado> kv : conversasPrivadas.entrySet()) {
-                if (kv.getKey().equals(message.getNameReserved())) {
+                if (kv.getKey().equals(message.getName())) {
                     kv.getValue().receive(message);
                     return;
                 }
@@ -127,23 +127,23 @@ public class ClienteFrame extends TelaChat {
             ChatPrivado chatPrivado = new ChatPrivado(message, this.service, this.conversasPrivadas);
             chatPrivado.setVisible(true);
             chatPrivado.receive(message);
-            
+
             conversasPrivadas.put(message.getName(), chatPrivado);
-            
+
         } else {
             this.txtAreaReceive.append(montarInfoMensagem(message, null));
         }
-        
+
     }
-    
+
     public void receiveArquivo(ChatMessage message) throws FileNotFoundException, IOException {
         FileOutputStream fos;
-        
+
         File criarPasta = new File("arquivos");
         String caminho = criarPasta.getAbsolutePath() + "/";
-        
+
         if (criarPasta.exists()) {
-            
+
             fos = new FileOutputStream(caminho + message.getNomeArquivo());
             fos.write(message.getConteudoArquivo());
             fos.close();
@@ -156,15 +156,15 @@ public class ClienteFrame extends TelaChat {
             Runtime.getRuntime().exec("explorer " + criarPasta.getAbsolutePath());
         }
     }
-    
+
     private void connected(ChatMessage message) {
-        
+
         if (message.getText().equals("NO")) {
             this.txtName.setText("");
             alertaErro("Não foi possível conectar, nome de usuário já existe", "Erro ao conectar");
             return;
         }
-        
+
         alerta("Você está conectado ao chat", "Conectado com sucesso");
         this.message = message;
         this.btnConectar.setEnabled(false);
@@ -175,13 +175,13 @@ public class ClienteFrame extends TelaChat {
         this.btnLimpar.setEnabled(true);
         this.btnSelecionarArquivo.setEnabled(true);
     }
-    
+
     private void disconnected() throws IOException {
-        
+
         if (this.message == null) {
             return;
         }
-        
+
         ChatMessage message = new ChatMessage();
         message.setName(this.message.getName());
         message.setAction(Action.DISCONECT);
@@ -426,19 +426,19 @@ public class ClienteFrame extends TelaChat {
 
     private void btnConectarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConectarActionPerformed
         String name = this.txtName.getText();
-        
+
         if (!name.isEmpty()) {
             try {
                 //Cria mensagem do tipo CONNECT
                 this.message = new ChatMessage();
                 this.message.setAction(Action.CONNECT);
                 this.message.setName(name);
-                
+
                 this.service = new ClienteService();
                 this.socket = service.connect();
-                
+
                 new Thread(new ListenerSocket(this.socket)).start();
-                
+
                 this.service.send(this.message);
             } catch (Exception e) {
                 alertaErro(e.getMessage(), "Erro ao conectar!");
@@ -461,16 +461,16 @@ public class ClienteFrame extends TelaChat {
 
     private void btnEnviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEnviarActionPerformed
         String text = this.txtAreaSend.getText();
-        
+
         if (text.isEmpty() && !temArquivo) {
             return;
         }
-        
+
         String name = this.message.getName();
         this.message = new ChatMessage();
-        
+
         int selected = this.listOnlines.getSelectedIndex();
-        
+
         if (selected > -1) {
             this.message.setNameReserved((String) this.listOnlines.getSelectedValue());
             this.message.setAction(Action.SEND_ONE);
@@ -478,17 +478,17 @@ public class ClienteFrame extends TelaChat {
         } else {
             this.message.setAction(Action.SEND_ALL);
         }
-        
+
         if (temArquivo) {
             this.message.setNomeArquivo(nomeArquivo);
             this.message.setConteudoArquivo(conteudoArquivo);
         }
-        
+
         this.message.setName(name);
         this.message.setText(text);
-        
+
         this.txtAreaReceive.append(montarInfoMensagem(message, "Você"));
-        
+
         this.service.send(message);
         this.txtAreaSend.setText("");
         this.temArquivo = false;
@@ -514,23 +514,23 @@ public class ClienteFrame extends TelaChat {
             JFileChooser chooser = new JFileChooser();
             chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
             chooser.setDialogTitle("Escolha o arquivo");
-            
+
             if (chooser.showOpenDialog(this) == JFileChooser.OPEN_DIALOG) {
                 File fileSelected = chooser.getSelectedFile();
-                
+
                 long tamanhoArquivo = fileSelected.length() / 1024;
-                
+
                 if (tamanhoArquivo > tamanhoPermitidoKB) {
                     JOptionPane.showMessageDialog(this, "Arquivo com tamanho maior que o permitido!", "Aviso",
                             JOptionPane.INFORMATION_MESSAGE);
                     return;
                 }
-                
+
                 byte[] bFile = new byte[(int) fileSelected.length()];
                 fis = new FileInputStream(fileSelected);
                 fis.read(bFile);
                 fis.close();
-                
+
                 this.conteudoArquivo = bFile;
                 this.nomeArquivo = fileSelected.getName();
                 lblNomeArquivo.setText(this.nomeArquivo);
@@ -538,7 +538,7 @@ public class ClienteFrame extends TelaChat {
                 txtAreaSend.append("\n" + nomeArquivo + "\n");
                 temArquivo = true;
             }
-            
+
         } catch (Exception e) {
             JOptionPane.showMessageDialog(this, "Erro " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
         }
@@ -550,7 +550,7 @@ public class ClienteFrame extends TelaChat {
             //int index = list.locationToIndex(evt.getPoint());
             // System.out.println("index: " + index);
             int selected = list.getSelectedIndex();
-            
+
             if (selected > -1) {
                 ChatMessage message = new ChatMessage();
                 String nameReserved = (String) this.listOnlines.getSelectedValue();
@@ -564,17 +564,17 @@ public class ClienteFrame extends TelaChat {
                  */
                 for (Map.Entry<String, ChatPrivado> kv : conversasPrivadas.entrySet()) {
                     if (kv.getKey().equals(nameReserved)) {
-                        kv.getValue().receive(message);
+                        //kv.getValue().receive(message);
                         return;
                     }
                 }
-                
+
                 ChatPrivado chat = new ChatPrivado(message, this.service, this.conversasPrivadas);
                 chat.setVisible(true);
                 conversasPrivadas.put(nameReserved, chat);
             }
         }
-        
+
 
     }//GEN-LAST:event_listOnlinesMouseClicked
 
